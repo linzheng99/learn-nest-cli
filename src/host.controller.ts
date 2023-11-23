206bfff2492ebcff9fdaa7a46349327df8fbb279
linzheng <alidlg9045@126.com>
2023-11-23 15:20:47 +0800

    learn: 浅浅了解 Nest 装饰器

--- src/host.controller.ts
+++ src/host.controller.ts
@@ -1,9 +1,69 @@
-import { Controller, Get, HostParam } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HostParam,
+  HttpCode,
+  Next,
+  Redirect,
+  Render,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Controller({ host: ':host.0.0.1', path: 'host' })
 export class HostController {
   @Get('bbb')
-  hello() {
-    return 'hello';
+  hello(@HostParam('host') host: string) {
+    return host;
+  }
+
+  @Get('ccc')
+  ccc(@Req() req: Request) {
+    console.log(req.hostname);
+    console.log(req.url);
+  }
+
+  @Get('ddd')
+  ddd(@Res({ passthrough: true }) res: Response) {
+    // res.end('ddd'); // 自己回应响应
+    return 'ddd';
+  }
+
+  // 处理同一个了路由 - next() 会调用到下面这个handler
+  @Get('eee')
+  eee(@Next() next: NextFunction) {
+    console.log('handler1');
+    next();
+    return '111';
+  }
+
+  @Get('eee')
+  eee2() {
+    console.log('handler2');
+    return 'eee';
+  }
+
+  @Get('fff')
+  @HttpCode(222) // 修改 status
+  fff() {
+    return 'hello, 222';
+  }
+
+  @Get('ggg')
+  @Header('aaa', 'bbb') // 修改header请求头的内容
+  ggg() {
+    return 'hello, ggg';
+  }
+
+  @Get('hhh')
+  @Redirect('http://juejin.cn') // 重定向
+  hhh() {}
+
+  @Get('user')
+  @Render('user')
+  user() {
+    return { name: 'lin', age: 18 };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { LoginGuard } from './login.guard';
 import { TimeInterceptor } from './time.interceptor';
 import { ValidatePipe } from './validate.pipe';
 import { TestFilter } from './test.filter';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,13 +15,16 @@ async function bootstrap() {
     next();
     console.log('after');
   });
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   // useStaticAssets 需要create指定泛类<NestExpressApplication>才能使用
   // app.useStaticAssets('public', { prefix: '/static' }); // 上传文件
 
   // app.useGlobalGuards(new LoginGuard()); // 全局启用guard
   // app.useGlobalInterceptors(new TimeInterceptor()); // 全局启用Interceptor
   // app.useGlobalPipes(new ValidatePipe()); // 全局启用Pipe
-  app.useGlobalFilters(new TestFilter()); // 全局启用Filters
+  // app.useGlobalFilters(new TestFilter()); // 全局启用Filters
   await app.listen(3000);
   // setTimeout(() => {
   //   app.close(); // 触发销毁逻辑
